1773e986ca9e4b34322a20e93d295ae2281bce4a
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2022-02-05 17:26:08 +0100

    Secure Orders and Restaurants API

--- Biz.FullFodder4u.Restaurants/Biz.FullFodder4u.Restaurants.API/Program.cs
+++ Biz.FullFodder4u.Restaurants/Biz.FullFodder4u.Restaurants.API/Program.cs
@@ -1,5 +1,8 @@
 using Biz.FullFodder4u.Restaurants.API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,20 @@ builder.Services.AddControllers(options =>
     options.Conventions.Add(
         new RouteTokenTransformerConvention(
             new SlugifyParameterTransformer()));
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,8 +45,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
