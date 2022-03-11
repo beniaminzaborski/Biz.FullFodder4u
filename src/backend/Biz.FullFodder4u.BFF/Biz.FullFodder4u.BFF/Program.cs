5e5a92db5346f0c23701711a357b28c43b8e7ddc
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2022-03-11 18:16:09 +0100

    BFF gateway configuration

--- src/backend/Biz.FullFodder4u.BFF/Biz.FullFodder4u.BFF/Program.cs
+++ src/backend/Biz.FullFodder4u.BFF/Biz.FullFodder4u.BFF/Program.cs
@@ -1,25 +1,97 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        //new WebHostBuilder()
+//        WebApplication.CreateBuilder(args).WebHost
+//            .UseKestrel()
+//            .UseContentRoot(Directory.GetCurrentDirectory())
+//            .ConfigureAppConfiguration((hostingContext, config) =>
+//            {
+//                config
+//                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+//                    .AddJsonFile("appsettings.json", true, true)
+//                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+//                    .AddJsonFile("ocelot.json")
+//                    .AddEnvironmentVariables();
+//            })
+//            .ConfigureServices(s =>
+//            {
+//                s.AddControllers();
+
+//                s.AddEndpointsApiExplorer()
+//                    .AddSwaggerGen();
+
+//                s.AddOcelot()
+//                    .AddPolly();
+//            })
+//            .ConfigureLogging((hostingContext, logging) =>
+//            {
+//                //add your logging
+//            })
+//            .UseIISIntegration()
+//            .Configure(app =>
+//            {
+//                if (app.Environment.IsDevelopment())
+//                {
+//                    app.UseSwagger();
+//                    app.UseSwaggerUI();
+//                }
+
+//                app.UseHttpsRedirection()
+//                    .UseAuthorization();
+
+//                //app.MapControllers();
+
+//                app.UseOcelot()
+//                    .Wait();
+//            })
+//            .Build()
+//            .Run();
+//    }
+//}
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", true, true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddJsonFile("ocelot.json")
+        .AddEnvironmentVariables();
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+//builder.Services.AddControllers();
+
+//builder.Services
+//    .AddEndpointsApiExplorer()
+//    .AddSwaggerGen();
+
+builder.Services
+    .AddOcelot()
+    .AddPolly();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection()
+//    .UseAuthorization();
 
-app.UseAuthorization();
+//app.MapControllers();
 
-app.MapControllers();
+app.UseOcelot();
 
 app.Run();
